5e13316163afa51b39c4924b8e8868f4a608c614
unatis <ygpanzer@gmail.com>
2025-11-29 17:14:58 +0200

    Initial commit

---
+++ DealManager/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Разрешаем отдавать статические файлы
+        app.UseStaticFiles();
+
+        // По желанию: можно сделать index.html доступным по корню
+        app.UseDefaultFiles(); // ищет index.html по умолчанию
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
